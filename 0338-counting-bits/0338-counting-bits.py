40b6f4eae3a0fb41f91f3aa9435220d6db7b46a9
MahalakshmiMurala <167948003+MahalakshmiMurala@users.noreply.github.com>
2025-10-03 09:43:01 +0530

    Time: 5 ms (87.73%), Space: 17.9 MB (48.42%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,12 +1,8 @@
 class Solution(object):
     def countBits(self, n):
-        """
-        :type n: int
-        :rtype: List[int]
-        """
-        ans = [0] * (n + 1)  # Initialize result array
+        ans = [0] * (n + 1) 
         
         for i in range(1, n + 1):
-            ans[i] = ans[i >> 1] + (i & 1)  # DP relation
+            ans[i] = ans[i >> 1] + (i & 1) 
             
         return ans
