583f9a22a635c937ace297af42f55b6e542e9030
MahalakshmiMurala <167948003+MahalakshmiMurala@users.noreply.github.com>
2025-10-03 09:50:12 +0530

    Time: 0 ms (100%), Space: 12.5 MB (9.39%) - LeetHub

---
+++ 0292-nim-game/0292-nim-game.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def canWinNim(self, n):
+        if n%4!=0:
+            return True
+        else:
+            return False
+        """
+        :type n: int
+        :rtype: bool
+        """
+        
