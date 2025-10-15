b94d17c2cdfc76131565e3dcdc908a74a926bf1f
MahalakshmiMurala <167948003+MahalakshmiMurala@users.noreply.github.com>
2025-10-15 15:32:55 +0530

    Time: 0 ms (100%), Space: 12.4 MB (79.01%) - LeetHub

---
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        jewels_set = set(jewels)
+        return sum(s in jewels_set for s in stones)
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        
