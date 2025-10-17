66ebf478d851236852dfd25fd2eb642970e7c2de
MahalakshmiMurala <167948003+MahalakshmiMurala@users.noreply.github.com>
2025-10-17 15:20:46 +0530

    Time: 0 ms (100%), Space: 12.3 MB (80.73%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def detectCapitalUse(self, word):
+        if word.isupper() or word.islower() or word.istitle():
+            return True
+        else:
+            return False
+        """
+        :type word: str
+        :rtype: bool
+        """
+        
