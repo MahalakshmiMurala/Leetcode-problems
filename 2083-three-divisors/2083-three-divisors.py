4ab5d6141d8974a7a8020f66a2a4d1e8a776307b
MahalakshmiMurala <167948003+MahalakshmiMurala@users.noreply.github.com>
2025-11-05 15:37:08 +0530

    Time: 4 ms (55.05%), Space: 12.5 MB (87.11%) - LeetHub

---
+++ 2083-three-divisors/2083-three-divisors.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    """
+    :type n: int
+    :rtype: bool
+    """
+    def isThree(self, n):
+        if n == 1 or n == 2:
+            return False
+        count = 2
+        for i in range(2,n//2 + 1):
+            if n % i == 0:
+                count += 1
+            if count > 3:
+                return False
+        return count == 3
+        
