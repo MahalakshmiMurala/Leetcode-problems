01a507f3c1f2af61c28ea4dbc4e83af5156d8bed
MahalakshmiMurala <167948003+MahalakshmiMurala@users.noreply.github.com>
2025-11-19 17:45:46 +0530

    Time: 7 ms (71.05%), Space: 20.8 MB (25.39%) - LeetHub

---
+++ 0089-gray-code/0089-gray-code.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def grayCode(self, n):
+        result = []
+        total_numbers = 1 << n
+        for i in range(total_numbers):
+            result.append(i ^ (i >> 1))
+
+        return result
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        
